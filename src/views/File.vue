01ecdd81151778910e125cf5b73c04fa0420c1cf
d4vss <godflod@gmx.at>
2023-09-27 23:56:21 +0200

    File Display Support

--- src/views/File.vue
+++ src/views/File.vue
@@ -1,64 +1,89 @@
 <template>
     <main ref="main">
-        <h1 class="panton">{{ file }} ({{ size }})</h1>
-        <a :href="url" class="link">Download</a>
+      <h1 class="panton"><span>{{ fileName }}</span> (<span>{{ fileSize }}</span>)</h1>
+      <div v-if="isImage">
+        <img :src="downloadUrl" alt="Image" />
+      </div>
+      <div v-else-if="isVideo">
+        <video controls :src="downloadUrl" width="400" height="300"></video>
+      </div>
+      <div v-else-if="isAudio">
+        <audio controls :src="downloadUrl"></audio>
+      </div>
+      <a class="link" :href="downloadUrl" target="_blank">Download</a>
     </main>
-</template>
-
-<script>
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { gsap } from 'gsap';
-
-const main = ref(null);
-
-onMounted(() => {
-  gsap.from(main.value.children, {
-    duration: 1,
-    y: -25,
-    opacity: 0,
-    stagger: 0.35,
-    ease: 'Power4.easeOut',
-  });
-});
-
-import router from '@/router';
-
-export default {
+  </template>
+  
+  <script>
+  import { ref, onMounted, computed } from 'vue';
+  import { useRoute } from 'vue-router';
+  
+  export default {
     setup() {
-        const route = useRoute();
-        const file = ref(null);
-        const url = ref(null);
-        const size = ref(null);
-
-        onMounted(async () => {
-            const response = await fetch(`https://dl.d4vss.net/${route.params.id}/url`);
-            const code = await response.status;
-            if (code == 404)
-                return router.push({ name: 'not-found' });
-            const splitText = (await response.text()).split('/');
-            file.value = splitText[splitText.length - 1];
-            url.value = `https://dl.d4vss.net/${route.params.id}/download`;
-
-            const response2 = await fetch(`https://dl.d4vss.net/${route.params.id}/size`);
-            size.value = await response2.text();
-        });
-
-        return {
-            file,
-            url,
-            size,
+      const route = useRoute();
+      const fileName = ref('');
+      const fileSize = ref('');
+      const downloadUrl = ref('');
+  
+      onMounted(async () => {
+        const response = await fetch(`https://dl.d4vss.net/${route.params.id}`);
+        const payload = await response.json();
+  
+        while (payload.fileName == null) {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
         }
-    }
-}
-</script>
-
-<style scoped>
-main {
+  
+        fileName.value = payload.fileName;
+        fileSize.value = payload.fileSize;
+        downloadUrl.value = payload.downloadUrl;
+      });
+  
+      // Determine if it's an image, video, or audio based on file extension
+      const isImage = computed(() => {
+        const extensions = ['.png', '.jpg', '.jpeg', '.gif'];
+        const fileExtension = fileName.value.split('.').pop().toLowerCase();
+        return extensions.includes(`.${fileExtension}`);
+      });
+  
+      const isVideo = computed(() => {
+        const extensions = ['.mp4', '.webm', '.ogg'];
+        const fileExtension = fileName.value.split('.').pop().toLowerCase();
+        return extensions.includes(`.${fileExtension}`);
+      });
+  
+      const isAudio = computed(() => {
+        const extensions = ['.mp3', '.wav'];
+        const fileExtension = fileName.value.split('.').pop().toLowerCase();
+        return extensions.includes(`.${fileExtension}`);
+      });
+  
+      return {
+        fileName,
+        fileSize,
+        downloadUrl,
+        isImage,
+        isVideo,
+        isAudio,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  main {
     text-align: center;
-}
-
-a {
+  }
+  
+  h1 {
+    word-break: break-all;
+  }
+  
+  img, video, audio {
+    max-width: 100%;
+    height: auto;
+  }
+  
+  a {
     display: inline-block;
     padding: 1rem 2rem;
     border-radius: 0.5rem;
@@ -68,5 +93,6 @@ a {
     font-size: 1.5rem;
     font-weight: 700;
     transition: all 0.2s ease;
-}
-</style>
+  }
+  </style>
+  
